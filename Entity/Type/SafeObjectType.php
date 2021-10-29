054800f4cf39388f66c2da33430179fb984e3e47
Tim <t.smit@workplacebuddy.com>
2021-10-29 15:49:54 +0200

    Remove deprecated `commented` use for Doctrine type

--- Entity/Type/SafeObjectType.php
+++ Entity/Type/SafeObjectType.php
@@ -2,6 +2,7 @@
 
 namespace JMS\JobQueueBundle\Entity\Type;
 
+use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Types\ObjectType;
 
 class SafeObjectType extends ObjectType
@@ -9,6 +10,11 @@ class SafeObjectType extends ObjectType
     public function getSQLDeclaration(array $fieldDeclaration, \Doctrine\DBAL\Platforms\AbstractPlatform $platform)
     {
         return $platform->getBlobTypeDeclarationSQL($fieldDeclaration);
+    }
+
+    public function requiresSQLCommentHint(AbstractPlatform $platform): bool
+    {
+        return true;
     }
 
     public function getName()
